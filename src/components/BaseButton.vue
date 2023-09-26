e4ba0872c49543e8aa7a12c490dc5c9c10aac803
Aleksandr <soft@vhlv.ru>
2023-09-26 18:22:00 +0300

    feat: create BaseButton

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,59 @@
+<template>
+  <button
+    :type="typeButton"
+    :class="className"
+    :name="nameButton"
+    class="button"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    typeButton: {
+      type: String,
+      default: '',
+    },
+    className: {
+      type: String,
+      default: '',
+    },
+    nameButton: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.button {
+  @include flexible();
+  @include button();
+  border-radius: 100px;
+  font-size: 14px;
+  font-weight: 1.7;
+  font-weight: 400;
+  padding: 10px 20px;
+  max-width: max-content;
+
+  &_add {
+    color: $colors-primary;
+    border: 2px solid $colors-primary;
+    background-color: $colors-white;
+  }
+
+  &_save {
+    color: $colors-white;
+    background-color: $colors-primary;
+  }
+
+  &_delete {
+    color: $colors-primary;
+    background-color: $colors-white;
+    padding: 0;
+  }
+}
+</style>
