61056a992d29497fea751950da75347750897398
Aleksandr <soft@vhlv.ru>
2023-09-30 15:33:12 +0300

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './index.css';
-import Vue from 'vue';
-import App from './App.vue';
-
-Vue.config.productionTip = false;
-
-new Vue({
-  render: h => h(App)
-}).$mount('#app');
--- src/utils/images.js
+++
@@ -1,8 +0,0 @@
-const images = require.context('../assets/images', true, /\.((png)|(jpe?g)|(gif)|(svg))$/);
-const imagesMap = {};
-
-images.keys().forEach(key => {
-  imagesMap[key] = images(key);
-});
-
-export default imagesMap;
